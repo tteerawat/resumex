8f799ef24c2e120a8e4acd090b94e01606f32e15
Teerawat Lamanchart <teerawat.lamanchart@gmail.com>
2021-03-21 23:33:56 +0700

    Remove unused code

--- lib/resumex.ex
+++ lib/resumex.ex
@@ -1,18 +1,2 @@
 defmodule Resumex do
-  @moduledoc """
-  Documentation for `Resumex`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Resumex.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/resumex_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ResumexTest do
-  use ExUnit.Case
-  doctest Resumex
-
-  test "greets the world" do
-    assert Resumex.hello() == :world
-  end
-end
