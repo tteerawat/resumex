751d6788b02249c2f818e1f673e6e9ca8edbd162
Teerawat Lamanchart <teerawat.lamanchart@gmail.com>
2021-03-21 09:58:54 +0700

    Initial commit

---
+++ lib/resumex.ex
@@ -0,0 +1,18 @@
+defmodule Resumex do
+  @moduledoc """
+  Documentation for `Resumex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Resumex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/resumex_test.exs
@@ -0,0 +1,8 @@
+defmodule ResumexTest do
+  use ExUnit.Case
+  doctest Resumex
+
+  test "greets the world" do
+    assert Resumex.hello() == :world
+  end
+end
